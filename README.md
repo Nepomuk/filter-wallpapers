cbe0523ec8fef82cb32fa0c51c31d752d7e8ddf4
André Goerres <andre.goerres+git@gmail.com>
2013-11-20 22:40:28 +0100

    First working version including a readme.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-filter-wallpapers
-=================
+Filter Images
+=============
 
-Filter the automatically downloaded wallpapers from reddit via IFTTT
+This script filters the images inside a folder and checks for dimensions. This is helpful, if you run an automated download script and want to get rid of small images.
+
+### Requirements
+This script is based on python, obviously you need that (python-2.7 works). You also need the following python packages:
+
+    PIL (python imaging library)
+
+### Options
+Inside the file, there are some options you should have a look at:
+
+* **`minDim`**: a tuple for (width, height)
+* **`useExactDim`**: *True*: match the `minDim` exactly, *False*: use `minDim` as a lower limit
+* **`useAR`**: Should we match to the aspect ratio from `minDim`?
+* **`imagePath`**: Path to the images. Remember the trailing /
+* **`moveImproper`**: Move images not matching the criteria instead of deleting them.
+* **`improperPath`**: If the above is true, move the improper images.
+
+### Example use-case
+I run an [IFTTT recipe](https://ifttt.com/recipes/129664) that downloads pictures posted in the subreddit [/r/EarthPorn](http://www.reddit.com/r/EarthPorn) into a Dropbox folder (thanks [@senfi](https://github.com/senfi)!). Because I want to use these pictures as wallpaper/screensaver images, I have to get rid of all those that are to small.
+The IFTTT script runs instantly, so I put this line in my crontab to run the script every 30 minutes:
+
+    30/2 *  *   *   *   andre    python /Users/andre/Development/git/filter-wallpapers/filter.py
---
+++ filter.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
+#
+#  Filter wallpapers by size/ratio
+# ----------------------------------
+#
+# With this IFTTT recipe you can automatically download images posted to a
+# specific subreddit on reddit.com. Unfortunately, this also includes small
+# images or those out of your required aspect ratio.
+# This small tool loops through all the images and deletes/moves the ones with
+# the wrong aspect ratio and/or those too small.
+
+# Configuration Array
+# Note for the paths: use trailing slash (/)!
+_conf = {
+    'minDim': (1680, 1050), # (width, height)
+    'useExactDim': False,   # True: exact dimensions, False: minDim is lower limit
+    'useAR': False,         # use the aspect ratio from the minimum dimensions
+    'imagePath': '/Users/andre/Dropbox/IFTTT/reddit/EarthPorn/',
+    'moveImproper': True,   # True moves, False deletes immediately
+    'improperPath': '/Users/andre/Dropbox/IFTTT/reddit/EarthPorn/improper/',
+}
+
+# import stuff
+import os               # file handling
+import re               # regular expression
+from PIL import Image   # handling images
+
+
+def handleImproper(file):
+    if _conf['moveImproper']:
+        if not os.path.exists(_conf['improperPath']):
+            os.makedirs(_conf['improperPath'], 0755)
+        os.rename(_conf['imagePath'] + file, _conf['improperPath'] + file)
+        print "move   {0}".format(file)
+    else:
+        os.remove(_conf['imagePath'] + file)
+        print "delete {0}".format(file)
+
+
+# the main function, obviously
+def main():
+    for filename in os.listdir(_conf['imagePath']):
+        filepath = _conf['imagePath'] + filename
+        if os.path.isdir(filepath):
+            continue
+        if re.match("\.jpg$", filename) is not None:
+            continue
+
+        image = Image.open(filepath)
+
+        # check for minimum/exact dimensions
+        if _conf['useExactDim']:
+            if image.size[0] != _conf['minDim'][0] or image.size[1] != _conf['minDim'][1]:
+                # print "delete (wrongDim) {0}".format(filename)
+                handleImproper(filename)
+                print
+        else:
+            if image.size[0] < _conf['minDim'][0] or image.size[1] < _conf['minDim'][1]:
+                # print "delete (minDim)   {0}".format(filename)
+                handleImproper(filename)
+                continue
+
+        # check for exact aspect ratio
+        if _conf['useAR']:
+            AR_req = float(_conf['minDim'][0]) / float(_conf['minDim'][1])
+            AR_img = float(image.size[0]) / float(image.size[1])
+            if AR_req != AR_img:
+                # print "delete (wrongAR)  {0}".format(filename)
+                handleImproper(filename)
+                continue
+
+        print "keep   {0}".format(filename)
+
+
+if __name__ == '__main__':
+    main()
